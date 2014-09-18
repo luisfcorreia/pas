4bf7caa2b438024e8979a09a55fe849992c83e70
Luis Correia <buga@loide.net>
2014-09-19 00:43:25 +0100

    added show webpage from data in file (created by cron)

--- view.py
+++ view.py
@@ -1,14 +1,34 @@
 import web
-#import db
 import config
+
+stuff = []
+
+def	readfile():
+	
+	data = ""
+	f = open('list.txt', 'r')
+	# read stuff from file
+	while True:
+		d1 = f.readline().strip()
+		d2 = f.readline().strip()
+		d3 = f.readline().strip()
+		
+		# idiotic EOF test
+		if not d3:
+			break
+			
+		stuff.append((d1, d2, d3))
+		
 
 t_globals = dict(
   datestr=web.datestr,
 )
 render = web.template.render('templates/', cache=config.cache, globals=t_globals)
 render._keywords['globals']['render'] = render
+readfile()
 
+
+		
 def listing():
-    #l = db.listing(**k)
-    #return render.listing(l)
-    return render.listing()
+
+	return render.listing(stuff)
