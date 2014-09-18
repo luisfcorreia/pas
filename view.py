f781edd84a8a69a9882a73e372e99993d185fdcb
Luis Correia <buga@loide.net>
2014-09-18 15:40:18 +0100

    Update view.py

--- view.py
+++ view.py
@@ -1,22 +1,4 @@
 import web
-import view, config
-from view import render
-
-urls = (
-    '/', 'index'
-)
-
-class index:
-    def GET(self):
-        return render.base(view.listing())
-
-if __name__ == "__main__":
-    app = web.application(urls, globals())
-    app.internalerror = web.debugerror
-    app.run()
-
-[dmarc@dmarc pas]$ cat view.py
-import web
 #import db
 import config
 
